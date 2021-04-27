c26bac2616af265453f109fd6b997695868a428a
James Brown <james-dev@jamh.ai>
2021-04-27 15:44:51 -0700

    header and basic logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,41 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `lightslategray`,
+      background: '#bdc3c7', /* fallback for old browsers */
+      background: '-webkit-linear-gradient(to right, #bdc3c7, #2c3e50)' /* Chrome 10-25, Safari 5.1-6 */,
+      background: 'linear-gradient(to right, #bdc3c7, #2c3e50)',
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        // maxWidth: 960,
+        // padding: `1.45rem 1.0875rem`,
+        // margin: `0 auto`,
+        width: '200px',
+        height: '135px',
+        marginLeft: '42vw',
+        borderRadius: '40px',
+
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <StaticImage
+        src="../images/cmc-logo.png"
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{
+          marginTop: '10px',
+          borderRadius: '15px',
+          opacity: 0.1
+        }}
+      />
+      {/* <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
@@ -26,7 +45,7 @@ const Header = ({ siteTitle }) => (
         >
           {siteTitle}
         </Link>
-      </h1>
+      </h1> */}
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,29 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+    {/* <div> */}
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    {/* <div> */}
+
     <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
+      src="../images/balcony.png"
+      style={{
+        width: '100vw',
+        quality: 95
+      }}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
+    {/* </div> */}
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
+    {/* </div> */}
   </Layout>
 )
 
